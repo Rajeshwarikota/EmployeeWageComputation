83c930c7fd5eba696f522edeb729a82188300d02
Rajeshwarikota <kotarajeswari003.com>
2023-02-12 15:38:57 +0530

    [Rajeshwari] Adding Employee wage using switch case

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -20,20 +20,20 @@ namespace EmployeeWageComputation
             Random random = new Random();
             int empAttendance = random.Next(0, 3);
             //UC3-Add Part Time Employee
-            if (empAttendance == isFullTime)
+            //UC4-Using switch case
+            switch (empAttendance)
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 8;
-
-            }
-            else if (empAttendance == isPartTime)
-            {
-                Console.WriteLine("Employee is PartTime");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
+                case isFullTime:
+                    Console.WriteLine("Employee is FullTime");
+                    empHrs = 8;
+                    break;
+                case isPartTime:
+                    Console.WriteLine("Employee is PartTime");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    break;
             }
             //UC2- Employee Daily Wage
             empDailyWage = empWagePerHr * empHrs;
