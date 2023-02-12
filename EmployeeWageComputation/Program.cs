66c02a86130399b9f971313d4f8497b5c86c928b
Rajeshwarikota <kotarajeswari003.com>
2023-02-12 17:37:56 +0530

    [Rajeshwari] Adding Employee wages for each companies

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -16,52 +16,21 @@ namespace EmployeeWageComputation
             Console.WriteLine("....EMP WAGE COMPUTATION....");
 
             //UC7-Use Class Method and Variables
-            Program obj = new Program();
-            obj.EmpWage("HCL", 30, 16, 60);
-            obj.EmpWage("TCS", 20, 15, 80);
-            obj.EmpWage("BRIDGELAB", 15, 18, 100);
-            Console.ReadLine();
-        }
             //UC8-Multiple companies
-            void EmpWage(string company, int empWagePerHr, int empWorkinDaysPerMonth, int empTotalWorkingHrs)
-            {
-                int empHrs = 0;
-                int totalEmpHrs = 0;
-                int empMontlyWage = 0;
-                int totalWorkingDays = 0;
+            EmpWageBuilder Tata = new EmpWageBuilder("HCL", 30, 16, 60);
+            EmpWageBuilder Google = new EmpWageBuilder("TCS", 20, 15, 80);
+            EmpWageBuilder Apple = new EmpWageBuilder("BRIDGELAB", 15, 18, 100);
 
-                //UC1- Employee Attendance
-                Random random = new Random();
-                //UC6-MaxHrs
-                while (totalWorkingDays < empWorkinDaysPerMonth && totalEmpHrs < empTotalWorkingHrs)
+            Tata.EmpWage();
+            Console.WriteLine(Tata.ToString());
+            Google.EmpWage();
+            Console.WriteLine(Apple.ToString());
+            Apple.EmpWage();
+            Console.WriteLine(Google.ToString());
 
-                {
-                    int empAttendance = random.Next(0, 3);
-                    //UC3- Add Part Time Employee
-                    //UC4- Switch Case
-                    switch (empAttendance)
-                    {
-                        case isFullTime:
-                            Console.WriteLine("Employee is FullTime");
-                            empHrs = 8;
-                            break;
-                        case isPartTime:
-                            Console.WriteLine("Employee is PartTime");
-                            empHrs = 4;
-                            break;
-                        default:
-                            Console.WriteLine("Employee is Absent");
-                            break;
-                    }
-                    totalWorkingDays++;
-                    totalEmpHrs += empHrs;
-                }
-                //UC2- Employee Daily Wage
-                //UC5- Employee Monthly Wage
-                empMontlyWage = empWagePerHr * totalEmpHrs;
-                Console.WriteLine("Company : " + company);
-                Console.WriteLine("Total Number of Hours is  " + totalEmpHrs + ", Total Number of days is " + totalWorkingDays + ", Montly Wage of Emplyoee is " + empMontlyWage);
-            }
+            Console.ReadLine();
+
+        }
         }
     }
 
