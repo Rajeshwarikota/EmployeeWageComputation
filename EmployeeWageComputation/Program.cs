25e4583fb0ec5334407405d1c7c92548d5c5791b
Rajeshwarikota <kotarajeswari003.com>
2023-02-12 15:23:59 +0530

    [Rajeshwari] Adding Employee attendance

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -11,6 +11,20 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("....EMP WAGE COMPUTATION....");
+
+            int isPresent = 1;
+            //UC1- Employee Attendance
+            Random random = new Random();
+            int empAttendance = random.Next(0, 2);
+
+            if (empAttendance == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
             Console.ReadLine();
         }
     }
